703b1eaeb2c09996eed91fdd110d59b4b2429324
RogersMath <116944378+RogersMath@users.noreply.github.com>
2024-06-30 10:19:41 -0400

    Create harvestMode.js

---
+++ harvestMode.js
@@ -0,0 +1,44 @@
+import { gameState } from './gameState.js';
+import { generateProblem } from './problem.js';
+import { isPrime } from './utils.js';
+import { updateDisplay, displayAnswers } from './ui.js';
+import { playSound } from './sound.js';
+import { checkVictory } from './main.js';
+
+function startHarvestMode() {
+    const { problem, answer } = generateProblem();
+    document.getElementById('problem').textContent = problem;
+    displayAnswers(answer);
+}
+
+function checkAnswer(userAnswer, correctAnswer) {
+    if (userAnswer === correctAnswer) {
+        playSound('correctSound');
+        let researchGain = 1;
+        researchGain += gameState.discoveries.coreProperties * 3;
+        if (gameState.discoveries.orderOfOperations > 0) {
+            researchGain *= 2;
+        }
+        gameState.research += researchGain;
+        gameState.correctProblems++;
+        
+        if (isPrime(Math.abs(correctAnswer))) {
+            let primeGain = 1 + gameState.discoveries.increasedComplexity * 2;
+            gameState.primes += primeGain;
+            playSound('primeHarvestSound');
+            if (gameState.discoveries.factoring > 0 && Math.random() < 0.5) {
+                gameState.primes += primeGain;
+            }
+        }
+    } else {
+        playSound('incorrectSound');
+        if (gameState.research > 0) {
+            gameState.research -= 1;
+        }
+    }
+    updateDisplay();
+    startHarvestMode();
+    checkVictory();
+}
+
+export { startHarvestMode, checkAnswer };
