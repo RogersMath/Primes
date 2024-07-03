273aa2c204f9b298918916409a29bb776af3dbd7
RogersMath <116944378+RogersMath@users.noreply.github.com>
2024-07-03 09:04:21 -0400

    Update gameState.js

--- gameState.js
+++ gameState.js
@@ -19,42 +19,11 @@ const gameState = {
     correctProblems: 0,
     soundEnabled: true,
     valueRange: 10,
-    completedBranches: 0
+    completedBranches: 0,
+    difficultyCounter: 1
 };
 
-const discoveryCosts = {
-    increasedComplexity: [10, 50, 500],
-    coreProperties: [200, 600, 1800],
-    orderOfOperations: [50, 250, 1250],
-    factoring: [50, 250, 1250],
-    modulo: [200, 600, 1800],
-    sieveOfEratosthenes: [50, 250, 1250],
-    compoundInterest: [50, 250, 1250]
-};
-
-const discoveryNames = {
-    increasedComplexity: 'Increased Complexity',
-    coreProperties: 'Core Properties',
-    orderOfOperations: 'Order of Operations',
-    factoring: 'Factoring',
-    modulo: 'Modulo',
-    sieveOfEratosthenes: 'Eratosthenes Sieve',
-    compoundInterest: 'Compound Interest'
-};
-
-const discoveryEffects = {
-    increasedComplexity: [
-        'Increase Prime harvest by 2, introduce subtraction and negative numbers',
-        'Increase Prime harvest by 2 more, introduce multiplication and division',
-        'Increase Prime harvest by 2 more, introduce absolute value problems'
-    ],
-    coreProperties: 'Gain 3 additional research per correct answer',
-    orderOfOperations: 'Double research gain and introduce complex problems',
-    factoring: '50% chance to gain an extra prime',
-    modulo: 'Gain research every second and boost Shard Prime',
-    sieveOfEratosthenes: 'Automatically harvest primes periodically',
-    compoundInterest: '+1% bonus to total Research at the end of each round'
-};
+// Rest of the file remains the same...
 
 function initializeGameState() {
     Object.assign(gameState, {
@@ -78,7 +47,8 @@ function initializeGameState() {
         correctProblems: 0,
         soundEnabled: true,
         valueRange: 10,
-        completedBranches: 0
+        completedBranches: 0,
+        difficultyCounter: 1
     });
 }
 
